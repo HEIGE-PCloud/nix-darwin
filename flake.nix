4fe9b63e5fb1c657c839c23f7329204c1dcc783b
PCloud <heige.pcloud@outlook.com>
2024-12-14 22:12:13 +0000

    install vscode

--- flake.nix
+++ flake.nix
@@ -21,80 +21,84 @@
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, homebrew-core, homebrew-cask, homebrew-bundle, ... }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.neovim
-	  pkgs.tmux
-	  pkgs.gh
-	  pkgs.git
-	];
-      
-      homebrew = {
-        enable = true;
-	casks = [
-	  "stats"
-	];
-	masApps = {
-          "AdGuard for Safari" = 1440147259;
-	};
+    let
+      configuration = { pkgs, ... }: {
+        # List packages installed in system profile. To search by name, run:
+        # $ nix-env -qaP | grep wget
+        nixpkgs.config.allowUnfree = true;
+        environment.systemPackages =
+          [
+            pkgs.neovim
+            pkgs.tmux
+            pkgs.gh
+            pkgs.git
+            pkgs.vscode
+            pkgs.nixpkgs-fmt
+          ];
+
+        homebrew = {
+          enable = true;
+          casks = [
+            "stats"
+          ];
+          masApps = {
+            "AdGuard for Safari" = 1440147259;
+          };
+        };
+
+        system.defaults = {
+          dock = {
+            autohide = true;
+            persistent-apps = [
+              "/Applications/Safari.app"
+            ];
+            orientation = "left";
+            tilesize = 32;
+          };
+          finder = {
+            AppleShowAllExtensions = true;
+            ShowPathbar = true;
+            ShowStatusBar = true;
+          };
+        };
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+
+        # Enable alternative shell support in nix-darwin.
+        # programs.fish.enable = true;
+
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 5;
+
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = "aarch64-darwin";
       };
-      
-      system.defaults = {
-        dock = {
-	  autohide = true;
-	  persistent-apps = [
-	    "/Applications/Safari.app"
-	  ];
-	  orientation = "left";
-	  tilesize = 32;
-	};
-	finder = {
-	  AppleShowAllExtensions = true;
-	  ShowPathbar = true;
-	  ShowStatusBar = true;
-	};
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#Junrongs-MacBook-Pro
+      darwinConfigurations."Junrongs-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              enable = true;
+              enableRosetta = true;
+              user = "jchen";
+              taps = {
+                "homebrew/homebrew-core" = homebrew-core;
+                "homebrew/homebrew-cask" = homebrew-cask;
+                "homebrew/homebrew-bundle" = homebrew-bundle;
+              };
+              mutableTaps = false;
+            };
+          }
+        ];
       };
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#Junrongs-MacBook-Pro
-    darwinConfigurations."Junrongs-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-      modules = [ 
-        configuration
-	nix-homebrew.darwinModules.nix-homebrew
-	{
-	  nix-homebrew = {
-	    enable = true;
-	    enableRosetta = true;
-	    user = "jchen";
-	    taps = {
-	      "homebrew/homebrew-core" = homebrew-core;
-	      "homebrew/homebrew-cask" = homebrew-cask;
-	      "homebrew/homebrew-bundle" = homebrew-bundle;
-	    };
-	    mutableTaps = false;
-	  };
-	}
-      ];
-    };
-  };
 }
