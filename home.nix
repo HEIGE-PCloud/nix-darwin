b907fe9e80907df12f31a11ee248d82f7bcbf0d3
PCloud <heige.pcloud@outlook.com>
2024-12-17 12:37:30 +0000

    fix home directory

--- home.nix
+++ home.nix
@@ -4,7 +4,7 @@
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username = "jchen";
-  home.homeDirectory = "/Users/jchen";
+  home.homeDirectory = /Users/jchen;
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
