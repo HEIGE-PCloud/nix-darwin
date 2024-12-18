4e92c470795dfd7d3df2846d4915451b9423a489
PCloud <heige.pcloud@outlook.com>
2024-12-18 16:41:15 +0000

    enableSudoTouchIdAuth

--- flake.nix
+++ flake.nix
@@ -52,6 +52,9 @@
 
         homebrew = {
           enable = true;
+          onActivation = {
+            cleanup = "zap";
+          };
           casks = [
             "stats"
             "eloston-chromium"
@@ -63,59 +66,55 @@
           };
         };
 
-        system.keyboard = {
-          enableKeyMapping = true;
-          remapCapsLockToEscape = true;
-        };
+        security.pam.enableSudoTouchIdAuth = true;
 
-        system.defaults = {
-          dock = {
-            autohide = true;
-            orientation = "left";
-            tilesize = 32;
+        system = {
+          keyboard = {
+            enableKeyMapping = true;
+            remapCapsLockToEscape = true;
           };
-          finder = {
-            AppleShowAllExtensions = true;
-            ShowPathbar = true;
-            ShowStatusBar = true;
-          };
-          WindowManager = {
-            EnableTiledWindowMargins = false;
-          };
-        };
 
-        system.activationScripts.applications.text =
-          let
-            env = pkgs.buildEnv {
-              name = "system-applications";
-              paths = config.environment.systemPackages;
-              pathsToLink = "/Applications";
+          defaults = {
+            dock = {
+              autohide = true;
+              orientation = "left";
+              tilesize = 32;
             };
-          in
-          pkgs.lib.mkForce ''
-            echo "setting up /Applications..." >&2
-            rm -rf /Applications/Nix\ Apps
-            mkdir -p /Applications/Nix\ Apps
-            find ${env}/Applications -mindepth 1 -type l -exec readlink '{}' + |
-            while read -r src; do
-              app_name=$(basename "$src")
-              echo "copying $src" >&2
-              ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-            done
-          '';
+            finder = {
+              AppleShowAllExtensions = true;
+              ShowPathbar = true;
+              ShowStatusBar = true;
+            };
+            WindowManager = {
+              EnableTiledWindowMargins = false;
+            };
+          };
+
+          stateVersion = 5;
+          configurationRevision = self.rev or self.dirtyRev or null;
+          activationScripts.applications.text =
+            let
+              env = pkgs.buildEnv {
+                name = "system-applications";
+                paths = config.environment.systemPackages;
+                pathsToLink = "/Applications";
+              };
+            in
+            pkgs.lib.mkForce ''
+              echo "setting up /Applications..." >&2
+              rm -rf /Applications/Nix\ Apps
+              mkdir -p /Applications/Nix\ Apps
+              find ${env}/Applications -mindepth 1 -type l -exec readlink '{}' + |
+              while read -r src; do
+                app_name=$(basename "$src")
+                echo "copying $src" >&2
+                ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+              done
+            '';
+        };
+
         # Necessary for using flakes on this system.
         nix.settings.experimental-features = "nix-command flakes";
-
-        # Enable alternative shell support in nix-darwin.
-        # programs.fish.enable = true;
-
-        # Set Git commit hash for darwin-version.
-        system.configurationRevision = self.rev or self.dirtyRev or null;
-
-        # Used for backwards compatibility, please read the changelog before changing.
-        # $ darwin-rebuild changelog
-        system.stateVersion = 5;
-
         # The platform the configuration will be used on.
         nixpkgs.hostPlatform = "aarch64-darwin";
       };
